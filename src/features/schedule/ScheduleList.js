5b3bb9894846f6b8d7dd213acf2299eda7a87b29
TranSangQST <72055149+TranSangQST@users.noreply.github.com>
2023-04-19 03:30:42 +0700

    [sang][feature][schedule]: update ScheduleList

--- src/features/schedule/ScheduleList.js
+++ src/features/schedule/ScheduleList.js
@@ -1,7 +1,8 @@
+import PropTypes from "prop-types";
+
 import { ArrowLeft as ArrowLeftIcon, ArrowRight as ArrowRightIcon } from "@mui/icons-material";
 import {
   Table,
-  TableBody,
   TableCell,
   TableContainer,
   TableHead,
@@ -18,35 +19,140 @@ import {
 } from "@mui/material";
 
 import formatDate from "date-and-time";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
-import CustomOverlay from "../../components/CustomOverlay";
-import scheduleServices from "../../services/scheduleServices";
 import { useFetchingStore } from "../../store/FetchingApiStore";
+import WithTimesLoaderWrapper from "./hocs/WithTimesLoaderWrapper";
+import staffServices from "../../services/staffServices";
+import { staffRoles } from "../../entities/Staff";
+// import timeOffServices from "../../services/timeOffServices";
+// import { createDateByDateAndTimeStr } from "../../utils/datetimeUtil";
 
-function ScheduleList() {
-  const [isFetchConfigSuccess, setIsFetchConfigSuccess] = useState(false);
-  const [times, setTimes] = useState([]);
+// function filterDoctorsByDayOfWeek(doctors, timesList, currentDate, timeOffs) {
+//   const timeListIds = Object.keys(timesList);
+
+//   const timesListObj = timesList.reduce((result, time) => {
+//     return {
+//       ...result,
+//       [time.id]: time
+//     };
+//   }, {});
+
+//   const filteredDoctors = doctors.map((doctor) => {
+//     const filteredSchedules = doctor.staffSchedules.filter((schedule) => {
+//       return schedule.dayOfWeek === currentDate.getDay();
+//     });
+
+//     const schedulesByTimeId = filteredSchedules.reduce((result, schedule) => {
+//       const temp = result[schedule.idTime] || [];
+
+//       /*************************************************************************
+//       // [schedule.idTime] là 1 array vì nếu có 2 schedule trùng timeId và dayOfWeek
+//       // Nhưng applyFrom và applyTo đè lên nhau
+//       // Nghĩa là current thuộc cả applyFrom và applyTo của 2 schedule trùng timeId và dayOfWeek
+//       return {
+//         ...result,
+//         [schedule.idTime]: [...temp, schedule]
+//       };
+
+//       Tạm thời ta sẽ lấy phần tử 0 trước, sau này sẽ fix lại sau
+
+//       *************************************************************************/
+
+//       const currentScheduleTimeStart = createDateByDateAndTimeStr(currentDate, timesListObj[schedule.idTime].timeStart);
+//       const currentScheduleTimeEnd = createDateByDateAndTimeStr(currentDate, timesListObj[schedule.idTime].timeEnd);
+
+//       let isTimeOff = false;
+//       timeOffs.forEach((timeOff) => {
+//         const timeOffStart = createDateByDateAndTimeStr(new Date(timeOff.date), timeOff.timeStart);
+//         const timeOffEnd = createDateByDateAndTimeStr(new Date(timeOff.date), timeOff.timeEnd);
+
+//         if (isBetweenAndNoEqual(currentScheduleTimeStart, timeOffStart, timeOffEnd)) {
+//           isTimeOff = true;
+//         } else if (isBetweenAndNoEqual(currentScheduleTimeEnd, timeOffStart, timeOffEnd)) {
+//           isTimeOff = true;
+//         } else if (isBetweenAndNoEqual(timeOffStart, currentScheduleTimeStart, currentScheduleTimeEnd)) {
+//           isTimeOff = true;
+//         } else if (isBetweenAndNoEqual(timeOffEnd, currentScheduleTimeStart, currentScheduleTimeEnd)) {
+//           isTimeOff = true;
+//         }
+//       });
+
+//       const scheduleWithIfIsTimeOff = { ...schedule, isTimeOff };
+
+//       return {
+//         ...result,
+//         [schedule.idTime]: [...temp, scheduleWithIfIsTimeOff]
+//       };
+//     }, {});
+
+//     // console.log("schedulesByTimeId: ", schedulesByTimeId);
+
+//     return { ...doctor, staffSchedules: [...filteredSchedules], schedulesByTimeId };
+//   });
+//   return filteredDoctors;
+// }
+
+function ScheduleList({ timesList }) {
   const [currentDate, setCurrentDate] = useState(new Date());
 
   const [doctors, setDoctors] = useState([]);
 
-  const { isLoading, fetchApi } = useFetchingStore();
+  // const [timeOffs, setTimeOffs] = useState([]);
 
-  const { t } = useTranslation("scheduleFeature", { keyPrefix: "schedule_list" });
+  const { fetchApi } = useFetchingStore();
+
+  const { t } = useTranslation("scheduleFeature", { keyPrefix: "ScheduleList" });
+
+  // const loadTimeOffs = async (doctorId) => {
+  //   const timeOffsOfDoctor = [];
+  //   await fetchApi(async () => {
+  //     const res = await timeOffServices.getTimeOffByDoctorId(doctorId, { from: currentDate, to: currentDate });
+  //
+  //     if (res.success) {
+  //       const timeOffsData = res.timeOffs;
+  //       // setTimeOffs(timeOffsData);
+  //       timeOffsOfDoctor.push(timeOffsData);
+  //       return { success: true, error: "" };
+  //     }
+  //     // setTimeOffs([]);
+  //     return { success: false, error: res.message };
+  //   });
+  //
+  //   return timeOffsOfDoctor;
+  // };
+
+  const loadAllTimeOffs = async () => {
+    // const allTimeOffs = [];
+    // for (const doctor of doctors) {
+    //   const timeOffsOfDoctor = await loadTimeOffs(doctor.id);
+    //   allTimeOffs.push({
+    //     [doctor.id]: timeOffsOfDoctor
+    //   });
+    // }
+    // setTimeOffs([]);
+  };
+
+  useEffect(() => {
+    loadAllTimeOffs();
+  }, [doctors]);
 
   const loadData = async () => {
     await fetchApi(async () => {
-      const res = {
-        succes: false,
-        messag: ""
+      const paramsObj = {
+        role: staffRoles.ROLE_DOCTOR,
+        limit: 200,
+        from: currentDate,
+        to: currentDate
       };
+      const res = await staffServices.getStaffList(paramsObj);
 
-      // console.log("load data res: ", res);
+      // let countData = 0;
+      // let staffsData = [];
 
       if (res.success) {
-        const doctorsData = res.doctors;
+        const doctorsData = res.staffs;
         setDoctors(doctorsData);
         return { success: true, error: "" };
       }
@@ -55,154 +161,123 @@ function ScheduleList() {
     });
   };
 
-  const rows = useMemo(() => {
-    if (doctors.length <= 0) {
-      return [];
-    }
-
-    const data = doctors?.map((doctor) => {
-      return {
-        ...doctor,
-        schedules: doctor?.schedules?.reduce((result, schedule) => {
-          return {
-            ...result,
-            [schedule?.idTime]: schedule
-          };
-        }, {})
-      };
-    });
-
-    // console.log("data: ", data);
-
-    return data;
-  }, [doctors]);
-
   useEffect(() => {
     loadData();
   }, [currentDate]);
 
-  const loadConfig = async () => {
-    await fetchApi(async () => {
-      const res = await scheduleServices.getTimeList();
+  // const rows = useMemo(() => {
+  //   // return filterDoctorsByDayOfWeek(doctors, timesList, currentDate, timeOffs);
+  //   return [];
+  // }, [doctors, timeOffs, timesList]);
 
-      if (res.success) {
-        setTimes(res.times);
-        return { success: true, error: "" };
-      }
-      setTimes([]);
-      return { success: false, error: res.message };
-    });
-
-    setIsFetchConfigSuccess(true);
-  };
-
-  useEffect(() => {
-    loadConfig();
-  }, []);
+  // console.log("doctors: ", doctors);
+  // console.log("timeOffs: ", timeOffs);
+  // console.log("rows: ", rows);
 
   return (
-    isFetchConfigSuccess && (
-      <Box>
-        <CustomOverlay open={isLoading} />
-        <Typography variant="h4" sx={{ mb: 4 }}>
-          {t("title")}
-        </Typography>
-        <Box sx={{ display: "flex", justifyContent: "flex-end", alignItems: "center", mb: 4 }}>
+    <Box>
+      <Typography variant="h4" sx={{ mb: 4 }}>
+        {t("title")}
+      </Typography>
+      <Box sx={{ display: "flex", justifyContent: "flex-end", alignItems: "center", mb: 4 }}>
+        <Box sx={{ display: "flex", justifyContent: "flex-end", alignItems: "center" }}>
           <Box sx={{ display: "flex", justifyContent: "flex-end", alignItems: "center" }}>
-            <Box sx={{ display: "flex", justifyContent: "flex-end", alignItems: "center" }}>
-              {formatDate.format(currentDate, "DD/MM/YYYY")}
-            </Box>
-            <IconButton
-              onClick={() => {
-                const newCurrentDate = new Date(currentDate);
-                newCurrentDate.setDate(newCurrentDate.getDate() - 6);
-                setCurrentDate(() => newCurrentDate);
-              }}
-            >
-              <ArrowLeftIcon fontSize="large" />
-            </IconButton>
-            <Button
-              variant="contained"
-              onClick={() => {
-                setCurrentDate(() => new Date());
-              }}
-              size="small"
-            >
-              {t("today")}
-            </Button>
-            <IconButton
-              onClick={() => {
-                const newCurrentDate = new Date(currentDate);
-                newCurrentDate.setDate(newCurrentDate.getDate() + 6);
-                setCurrentDate(() => newCurrentDate);
-              }}
-            >
-              <ArrowRightIcon fontSize="large" />
-            </IconButton>
+            {formatDate.format(currentDate, "DD/MM/YYYY")}
           </Box>
+          <IconButton
+            onClick={() => {
+              const newCurrentDate = new Date(currentDate);
+              newCurrentDate.setDate(newCurrentDate.getDate() - 1);
+              setCurrentDate(() => newCurrentDate);
+            }}
+          >
+            <ArrowLeftIcon fontSize="large" />
+          </IconButton>
+          <Button
+            variant="contained"
+            onClick={() => {
+              setCurrentDate(() => new Date());
+            }}
+            size="small"
+          >
+            {t("today")}
+          </Button>
+          <IconButton
+            onClick={() => {
+              const newCurrentDate = new Date(currentDate);
+              newCurrentDate.setDate(newCurrentDate.getDate() + 1);
+              setCurrentDate(() => newCurrentDate);
+            }}
+          >
+            <ArrowRightIcon fontSize="large" />
+          </IconButton>
         </Box>
-        <TableContainer component={Paper}>
-          <Table size="small" aria-label="a dense table">
-            <TableHead>
-              <TableRow>
-                <TableCell
-                  sx={{
-                    border: "1px solid rgba(0,0,0,0.2)"
-                  }}
-                />
-                {times?.map((time) => {
-                  return (
-                    <TableCell
-                      sx={{
-                        border: "1px solid rgba(0,0,0,0.2)",
-                        fontWeight: "600"
-                      }}
-                      key={time?.id}
-                      align="center"
-                    >
-                      {time?.timeStart}
-                    </TableCell>
-                  );
-                })}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map((row) => {
+      </Box>
+      <TableContainer component={Paper}>
+        <Table size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell
+                sx={{
+                  border: "1px solid rgba(0,0,0,0.2)"
+                }}
+              />
+              {timesList?.map((time) => {
                 return (
-                  <TableRow key={row.id}>
-                    <TableCell
-                      sx={{
-                        border: "1px solid rgba(0,0,0,0.2)",
-                        fontWeight: "600"
-                      }}
-                      component="th"
-                      scope="row"
-                    >
-                      {row?.name}
-                    </TableCell>
-                    {times?.map((time) => {
-                      // console.log("time?.id: ", time?.id);
-                      return (
-                        <TableCell
-                          key={time?.id}
-                          sx={{
-                            border: "1px solid rgba(0,0,0,0.2)"
-                          }}
-                          align="center"
-                        >
-                          {row?.schedules[time?.id]?.status}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
+                  <TableCell
+                    sx={{
+                      border: "1px solid rgba(0,0,0,0.2)",
+                      fontWeight: "600"
+                    }}
+                    key={time?.id}
+                    align="center"
+                  >
+                    {`${time?.timeStart.split(":")[0]}:${time?.timeStart.split(":")[1]}`}
+                  </TableCell>
                 );
               })}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Box>
-    )
+            </TableRow>
+          </TableHead>
+          {/* <TableBody>
+            {rows.map((row) => {
+              return (
+                <TableRow key={row.id}>
+                  <TableCell
+                    sx={{
+                      border: "1px solid rgba(0,0,0,0.2)",
+                      fontWeight: "600"
+                    }}
+                    component="th"
+                    scope="row"
+                  >
+                    {row?.name}
+                  </TableCell>
+                  {timesList?.map((time) => {
+                    // console.log("time?.id: ", time?.id);
+                    return (
+                      <TableCell
+                        key={time?.id}
+                        sx={{
+                          border: "1px solid rgba(0,0,0,0.2)"
+                        }}
+                        align="center"
+                      >
+                        {row?.schedules[time?.id]?.status}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+          </TableBody> */}
+        </Table>
+      </TableContainer>
+    </Box>
   );
 }
 
-export default ScheduleList;
+ScheduleList.propTypes = {
+  timesList: PropTypes.array.isRequired
+};
+
+export default WithTimesLoaderWrapper(ScheduleList);
