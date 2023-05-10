b04e3409d40c94eb7a761c0e9713d8df97ced469
TranSangQST <72055149+TranSangQST@users.noreply.github.com>
2023-05-10 12:06:12 +0700

    [sang][feature][schedule]: fix API Add Schedule

--- src/features/schedule/components/AddScheduleModal.js
+++ src/features/schedule/components/AddScheduleModal.js
@@ -13,6 +13,7 @@ import WithTimesLoaderWrapper from "../hocs/WithTimesLoaderWrapper";
 import { scheduleDayOfWeeks, scheduleSessions, scheduleTypes } from "../../../entities/Schedule/constant";
 import { useAppConfigStore } from "../../../store/AppConfigStore";
 import scheduleServices from "../../../services/scheduleServices";
+import CustomOverlay from "../../../components/CustomOverlay/CustomOverlay";
 
 function AddScheduleModal({ show, setShow, data, setData, handleAfterAddSchedule }) {
   const { handleSubmit, watch, control, trigger, reset } = useForm({
@@ -37,7 +38,7 @@ function AddScheduleModal({ show, setShow, data, setData, handleAfterAddSchedule
   const { t: tScheduleConstants } = useTranslation("scheduleEntity", { keyPrefix: "constants" });
   const { t: tInputValidate } = useTranslation("input", { keyPrefix: "validation" });
 
-  const { fetchApi } = useFetchingStore();
+  const { isLoading, fetchApi } = useFetchingStore();
   const { locale } = useAppConfigStore();
   const theme = useTheme();
 
@@ -142,268 +143,272 @@ function AddScheduleModal({ show, setShow, data, setData, handleAfterAddSchedule
   };
 
   return (
-    <CustomModal
-      show={show}
-      setShow={setShow}
-      data={data}
-      setData={setData}
-      title={t("title")}
-      submitBtnLabel={t("button.save")}
-      onSubmit={handleSubmit(handleAddSchedule)}
-      width="80%"
-    >
-      <Box
-        sx={{
-          width: "100%",
-          px: 2,
-          overflow: "scroll",
-          maxHeight: 450
-        }}
+    <>
+      <CustomOverlay open={isLoading} />
+      <CustomModal
+        show={show}
+        setShow={setShow}
+        data={data}
+        setData={setData}
+        title={t("title")}
+        submitBtnLabel={t("button.save")}
+        onSubmit={handleSubmit(handleAddSchedule)}
+        width="80%"
       >
-        <Grid
-          container
-          spacing={3}
+        <Box
           sx={{
-            mb: 2
+            width: "100%",
+            px: 2,
+            py: 2,
+            overflow: "scroll",
+            maxHeight: 450
           }}
         >
-          <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
-            <CustomInput
-              control={control}
-              rules={{
-                required: tInputValidate("required")
-              }}
-              label={tSchedule("applyFrom")}
-              trigger={trigger}
-              name="applyFrom"
-              type="date"
-            />
-          </Grid>
-          <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
-            <CustomInput
-              control={control}
-              rules={{
-                required: `${tSchedule("applyTo")} ${tInputValidate("required")}`,
-                validate: (value) => {
-                  const { applyFrom } = watch();
-                  const applyTo = value;
+          <Grid
+            container
+            spacing={3}
+            sx={{
+              mb: 2
+            }}
+          >
+            <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
+              <CustomInput
+                control={control}
+                rules={{
+                  required: tInputValidate("required")
+                }}
+                label={tSchedule("applyFrom")}
+                trigger={trigger}
+                name="applyFrom"
+                type="date"
+              />
+            </Grid>
+            <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
+              <CustomInput
+                control={control}
+                rules={{
+                  required: `${tSchedule("applyTo")} ${tInputValidate("required")}`,
+                  validate: (value) => {
+                    const { applyFrom } = watch();
+                    const applyTo = value;
 
-                  if (applyFrom <= applyTo) {
-                    return true;
+                    if (applyFrom <= applyTo) {
+                      return true;
+                    }
+                    return tInputValidate("gte", {
+                      left: tSchedule("applyToLabel"),
+                      right: tSchedule("applyFromLabel")
+                    });
                   }
-                  return tInputValidate("gte", {
-                    left: tSchedule("applyToLabel"),
-                    right: tSchedule("applyFromLabel")
-                  });
-                }
-              }}
-              label={tSchedule("applyTo")}
-              trigger={trigger}
-              name="applyTo"
-              type="date"
-              isCustomError
-            />
+                }}
+                label={tSchedule("applyTo")}
+                trigger={trigger}
+                name="applyTo"
+                type="date"
+                isCustomError
+              />
+            </Grid>
           </Grid>
-        </Grid>
 
-        {watch().dataForm?.map((row, index) => {
-          const keyIndex = `dataForm-${index}`;
-          return (
-            <Box
-              key={keyIndex}
-              sx={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center"
-              }}
-            >
-              <Grid
-                container
-                spacing={3}
+          {watch().dataForm?.map((row, index) => {
+            const keyIndex = `dataForm-${index}`;
+            return (
+              <Box
+                key={keyIndex}
                 sx={{
-                  mb: 2
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center"
                 }}
               >
-                <Grid item xs={12} sm={12} md={6} lg={3} xl={3}>
-                  <CustomInput
-                    control={control}
-                    rules={{
-                      required: tInputValidate("required")
-                    }}
-                    label={tSchedule("expertise")}
-                    trigger={trigger}
-                    name={`dataForm[${index}].expertise`}
-                    type="select"
-                  >
-                    <Select
-                      renderValue={(selected) => {
-                        return expertiseListObj[selected].name;
-                      }}
-                    >
-                      {Object.keys(expertiseListObj).map((key) => {
-                        const item = expertiseListObj[key];
-
-                        return (
-                          <MenuItem key={item?.id} value={item?.id}>
-                            <Checkbox checked={watch().dataForm[index].expertise === item?.id} />
-                            <ListItemText primary={item?.name} />
-                          </MenuItem>
-                        );
-                      })}
-                    </Select>
-                  </CustomInput>
-                </Grid>
-                <Grid item xs={12} sm={12} md={6} lg={3} xl={3}>
-                  <CustomInput
-                    control={control}
-                    rules={{
-                      required: tInputValidate("required")
-                    }}
-                    label={tSchedule("type")}
-                    trigger={trigger}
-                    name={`dataForm[${index}].type`}
-                    type="select"
-                  >
-                    <Select
-                      renderValue={(selected) => {
-                        return scheduleTypeListObj[selected].label;
-                      }}
-                    >
-                      {Object.keys(scheduleTypeListObj).map((key) => {
-                        const item = scheduleTypeListObj[key];
-
-                        return (
-                          <MenuItem key={item?.value} value={item?.value}>
-                            <Checkbox checked={watch().dataForm[index].type === item?.value} />
-                            <ListItemText primary={item?.label} />
-                          </MenuItem>
-                        );
-                      })}
-                    </Select>
-                  </CustomInput>
-                </Grid>
-                <Grid item xs={12} sm={12} md={6} lg={3} xl={3}>
-                  <CustomInput
-                    control={control}
-                    rules={{
-                      required: tInputValidate("required")
-                    }}
-                    label={tSchedule("session")}
-                    trigger={trigger}
-                    name={`dataForm[${index}].session`}
-                    type="select"
-                  >
-                    <Select
-                      renderValue={(selected) => {
-                        return scheduleSessionListObj[selected].label;
-                      }}
-                    >
-                      {Object.keys(scheduleSessionListObj).map((key) => {
-                        const item = scheduleSessionListObj[key];
-
-                        return (
-                          <MenuItem key={item?.value} value={item?.value}>
-                            <Checkbox checked={watch().dataForm[index].session === item?.value} />
-                            <ListItemText primary={item?.label} />
-                          </MenuItem>
-                        );
-                      })}
-                    </Select>
-                  </CustomInput>
-                </Grid>
-                <Grid item xs={12} sm={12} md={6} lg={3} xl={3}>
-                  <CustomInput
-                    control={control}
-                    rules={{
-                      required: tInputValidate("required")
-                    }}
-                    label={tSchedule("repeatOn")}
-                    trigger={trigger}
-                    name={`dataForm[${index}].repeatOn`}
-                    type="select"
-                  >
-                    <Select
-                      multiple
-                      renderValue={(selected) => {
-                        if (Array.isArray(selected))
-                          return selected
-                            ?.map((cur) => {
-                              return scheduleDayOfWeekListObj[cur]?.label;
-                            })
-                            ?.join(", ");
-                        return selected;
-                      }}
-                    >
-                      {Object.keys(scheduleDayOfWeekListObj).map((key) => {
-                        const item = scheduleDayOfWeekListObj[key];
-
-                        return (
-                          <MenuItem key={item?.value} value={item?.value}>
-                            <Checkbox checked={watch().dataForm[index].repeatOn?.indexOf(item?.value) > -1} />
-                            <ListItemText primary={item?.label} />
-                          </MenuItem>
-                        );
-                      })}
-                    </Select>
-                  </CustomInput>
-                </Grid>
-              </Grid>
-
-              {watch().dataForm?.length > 1 && (
-                <IconButton
-                  onClick={() => {
-                    const newData = [...watch().dataForm];
-                    newData.splice(index, 1);
-
-                    reset({
-                      ...watch(),
-                      dataForm: [...newData]
-                    });
+                <Grid
+                  container
+                  spacing={3}
+                  sx={{
+                    mb: 2
                   }}
                 >
-                  <RemoveCircleIcon
-                    sx={{
-                      transform: "translateY(-50%)",
-                      color: theme.palette.error.light
-                    }}
-                  />
-                </IconButton>
-              )}
-            </Box>
-          );
-        })}
+                  <Grid item xs={12} sm={12} md={6} lg={3} xl={3}>
+                    <CustomInput
+                      control={control}
+                      rules={{
+                        required: tInputValidate("required")
+                      }}
+                      label={tSchedule("expertise")}
+                      trigger={trigger}
+                      name={`dataForm[${index}].expertise`}
+                      type="select"
+                    >
+                      <Select
+                        renderValue={(selected) => {
+                          return expertiseListObj[selected].name;
+                        }}
+                      >
+                        {Object.keys(expertiseListObj).map((key) => {
+                          const item = expertiseListObj[key];
 
-        <Button
-          variant="contained"
-          sx={{
-            backgroundColor: theme.palette.success.light
-          }}
-          startIcon={
-            <AddIcon
-              sx={{
-                color: theme.palette.success.contrastText
-              }}
-            />
-          }
-          onClick={() => {
-            reset({
-              ...watch(),
-              dataForm: [
-                ...watch().dataForm,
-                {
-                  type: "",
-                  expertise: "",
-                  session: "",
-                  repeatOn: []
-                }
-              ]
-            });
-          }}
-        >
-          {t("button.addRow")}
-        </Button>
-      </Box>
-    </CustomModal>
+                          return (
+                            <MenuItem key={item?.id} value={item?.id}>
+                              <Checkbox checked={watch().dataForm[index].expertise === item?.id} />
+                              <ListItemText primary={item?.name} />
+                            </MenuItem>
+                          );
+                        })}
+                      </Select>
+                    </CustomInput>
+                  </Grid>
+                  <Grid item xs={12} sm={12} md={6} lg={3} xl={3}>
+                    <CustomInput
+                      control={control}
+                      rules={{
+                        required: tInputValidate("required")
+                      }}
+                      label={tSchedule("type")}
+                      trigger={trigger}
+                      name={`dataForm[${index}].type`}
+                      type="select"
+                    >
+                      <Select
+                        renderValue={(selected) => {
+                          return scheduleTypeListObj[selected].label;
+                        }}
+                      >
+                        {Object.keys(scheduleTypeListObj).map((key) => {
+                          const item = scheduleTypeListObj[key];
+
+                          return (
+                            <MenuItem key={item?.value} value={item?.value}>
+                              <Checkbox checked={watch().dataForm[index].type === item?.value} />
+                              <ListItemText primary={item?.label} />
+                            </MenuItem>
+                          );
+                        })}
+                      </Select>
+                    </CustomInput>
+                  </Grid>
+                  <Grid item xs={12} sm={12} md={6} lg={3} xl={3}>
+                    <CustomInput
+                      control={control}
+                      rules={{
+                        required: tInputValidate("required")
+                      }}
+                      label={tSchedule("session")}
+                      trigger={trigger}
+                      name={`dataForm[${index}].session`}
+                      type="select"
+                    >
+                      <Select
+                        renderValue={(selected) => {
+                          return scheduleSessionListObj[selected].label;
+                        }}
+                      >
+                        {Object.keys(scheduleSessionListObj).map((key) => {
+                          const item = scheduleSessionListObj[key];
+
+                          return (
+                            <MenuItem key={item?.value} value={item?.value}>
+                              <Checkbox checked={watch().dataForm[index].session === item?.value} />
+                              <ListItemText primary={item?.label} />
+                            </MenuItem>
+                          );
+                        })}
+                      </Select>
+                    </CustomInput>
+                  </Grid>
+                  <Grid item xs={12} sm={12} md={6} lg={3} xl={3}>
+                    <CustomInput
+                      control={control}
+                      rules={{
+                        required: tInputValidate("required")
+                      }}
+                      label={tSchedule("repeatOn")}
+                      trigger={trigger}
+                      name={`dataForm[${index}].repeatOn`}
+                      type="select"
+                    >
+                      <Select
+                        multiple
+                        renderValue={(selected) => {
+                          if (Array.isArray(selected))
+                            return selected
+                              ?.map((cur) => {
+                                return scheduleDayOfWeekListObj[cur]?.label;
+                              })
+                              ?.join(", ");
+                          return selected;
+                        }}
+                      >
+                        {Object.keys(scheduleDayOfWeekListObj).map((key) => {
+                          const item = scheduleDayOfWeekListObj[key];
+
+                          return (
+                            <MenuItem key={item?.value} value={item?.value}>
+                              <Checkbox checked={watch().dataForm[index].repeatOn?.indexOf(item?.value) > -1} />
+                              <ListItemText primary={item?.label} />
+                            </MenuItem>
+                          );
+                        })}
+                      </Select>
+                    </CustomInput>
+                  </Grid>
+                </Grid>
+
+                {watch().dataForm?.length > 1 && (
+                  <IconButton
+                    onClick={() => {
+                      const newData = [...watch().dataForm];
+                      newData.splice(index, 1);
+
+                      reset({
+                        ...watch(),
+                        dataForm: [...newData]
+                      });
+                    }}
+                  >
+                    <RemoveCircleIcon
+                      sx={{
+                        transform: "translateY(-50%)",
+                        color: theme.palette.error.light
+                      }}
+                    />
+                  </IconButton>
+                )}
+              </Box>
+            );
+          })}
+
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: theme.palette.success.light
+            }}
+            startIcon={
+              <AddIcon
+                sx={{
+                  color: theme.palette.success.contrastText
+                }}
+              />
+            }
+            onClick={() => {
+              reset({
+                ...watch(),
+                dataForm: [
+                  ...watch().dataForm,
+                  {
+                    type: "",
+                    expertise: "",
+                    session: "",
+                    repeatOn: []
+                  }
+                ]
+              });
+            }}
+          >
+            {t("button.addRow")}
+          </Button>
+        </Box>
+      </CustomModal>
+    </>
   );
 }
 
