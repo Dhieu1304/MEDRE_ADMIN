42f26a7df35ee0413272f6cf5d4d07c3be244d2b
TranSangQST <72055149+TranSangQST@users.noreply.github.com>
2023-06-27 23:06:06 +0700

    Fix bug Add TimeOff in DoctorCalendar

--- src/entities/Staff/Staff.js
+++ src/entities/Staff/Staff.js
@@ -1,52 +1,7 @@
 class Staff {
-  constructor({
-    id = "",
-    username = "",
-    phoneNumber = "",
-    email = "",
-    password = "",
-    name = "",
-    image = "",
-    address = "",
-    gender = "",
-    dob = "",
-    role = "",
-    emailVerified = "",
-    phoneVerified = "",
-    healthInsurance = "",
-    blocked = false,
-    description = "",
-    education = "",
-    certificate = "",
-    createdAt = "",
-    updatedAt = "",
-    deletedAt = "",
-    expertises = []
-  } = {}) {
-    this.id = id;
-    this.username = username;
-    this.phoneNumber = phoneNumber;
-    this.email = email;
-    this.password = password;
-    this.name = name;
-    this.image = image;
-    this.address = address;
-    this.gender = gender;
-    this.dob = dob;
-    this.role = role;
-    this.emailVerified = emailVerified;
-    this.phoneVerified = phoneVerified;
-    this.healthInsurance = healthInsurance;
-    this.blocked = blocked;
-    this.description = description;
-    this.education = education;
-    this.certificate = certificate;
-    this.createdAt = createdAt;
-    this.updatedAt = updatedAt;
-    this.deletedAt = deletedAt;
-    this.expertises = [...expertises];
+  constructor(attrs) {
+    Object.assign(this, attrs);
   }
-
   static magicWord = () => "Staff";
 }
 
